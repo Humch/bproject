515105c2bf62c39bf37a1d05cf8238a13eb553a5
Humch <fabienschlegel@yahoo.fr>
2018-03-02 18:40:57 +0100

    enigma 2

--- bproject.js
+++ bproject.js
@@ -41,7 +41,7 @@ function timer() {
 var submitEnigma1 = document.getElementById("submit-enigma1");
 submitEnigma1.addEventListener("submit", enigmaOne, false);
 
-function goodAnswer() {
+function goodAnswer1() {
     $('#enigma-one').modal("hide")
     var a = document.getElementById("img-enigma-one");
     a.classList.remove("red-bg");
@@ -63,6 +63,37 @@ function enigmaOne() {
         z.classList.remove("red-text");
         z.classList.add("green-text");
         z.innerText = "Bonne réponse mais je ne suis pas vaincu !!!";
-        window.setTimeout(goodAnswer,3000);
+        window.setTimeout(goodAnswer1,3000);
+    }
+}
+
+/* Enigma 2*/
+
+var submitEnigma2 = document.getElementById("submit-enigma2");
+submitEnigma2.addEventListener("submit", enigmaTwo, false);
+
+function goodAnswer2() {
+    $('#enigma-two').modal("hide")
+    var a = document.getElementById("img-enigma-two");
+    a.classList.remove("red-bg");
+        a.classList.add("green-bg");
+        a.dataset.target = "";
+}
+
+
+function enigmaTwo() {
+    console.log("pop");
+    var x = document.forms["submit-enigma2"]["answer-enigma-2"].value;
+    if (x != "bob") {
+        console.log("bah !!!");
+        var y = document.getElementById("bad-enigma-2");
+        y.innerText = "Mauvaise réponse !! Essaie encore";
+    } else {
+        console.log("youpi");
+        var z = document.getElementById("bad-enigma-2");
+        z.classList.remove("red-text");
+        z.classList.add("green-text");
+        z.innerText = "Tu as gagné ce challenge !!!";
+        window.setTimeout(goodAnswer2,3000);
     }
 }
